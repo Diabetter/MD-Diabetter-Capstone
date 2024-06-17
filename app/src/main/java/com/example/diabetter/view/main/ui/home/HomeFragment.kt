867ae5879e0043cd9c40d45a3b6dd9a60081f52a
KahilAkbr <kahil.akbar@gmail.com>
2024-06-17 16:18:09 +0700

    UPDATE : menu today rating

--- app/src/main/java/com/example/diabetter/view/main/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/diabetter/view/main/ui/home/HomeFragment.kt
@@ -1,21 +1,23 @@
 package com.example.diabetter.view.main.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.diabetter.R
 import com.example.diabetter.databinding.FragmentHomeBinding
+import com.example.diabetter.databinding.TodayMenuBinding
+import com.example.diabetter.view.detail_menu.DetailMenuActivity
 import com.example.diabetter.view.main.ui.home.adapter.RecommendationMenuAdapter
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+    private lateinit var menuTodayMenuBinding: TodayMenuBinding
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +27,34 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
-
-        // Set up RecyclerView
-        val recyclerView = view.findViewById<RecyclerView>(R.id.rv_recommendation_menu)
-        recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        recyclerView.adapter = RecommendationMenuAdapter(5)  // Menampilkan 5 item
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        menuTodayMenuBinding = binding.menuToday
+//        val ratingBar = view.findViewById<RatingBar>(R.id.rating)
+//
+//        // Atur nilai rating
+//        ratingBar.rating = 3.65f
+
+        binding.rvRecommendationMenu.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        binding.rvRecommendationMenu.adapter = RecommendationMenuAdapter(5)
+
+        menuTodayMenuBinding.apply {
+            btnSeeDetail.setOnClickListener {
+                val intent = Intent(requireContext(), DetailMenuActivity::class.java)
+                startActivity(intent)
+            }
+            tvDetailMenuToday.setOnClickListener {
+                val intent = Intent(requireContext(), DetailMenuActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 
     override fun onDestroyView() {
